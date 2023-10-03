5385f46efb020a3b843c6ce69150afdb03746598
Danilo Suzano <danilosuzano0@gmail.com>
2023-10-03 15:10:16 -0300

    Commit Inicial

---
+++ JS/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+    $('#carrossel-imagens').slick({
+        autoplay: true,
+    });
+
+    $('.menu-hamburguer').click(function(){
+        $('nav').slideToggle();
+    })
+
+    $('#telefone').mask('(00) 00000-0000',{
+        placeholder: '( ) _____-____'
+    });
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            mensagem:{
+                required: true
+            },
+            veiculo: {
+                required: false
+            }
+        },
+        messages: {
+            nome: 'Por Favor insira seu nome.'
+        },
+        submitHandler: function(form){
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador){
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos){
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+
+    $('.lista-veiculos button').click(function(){
+        const destino = $('#contato');
+
+        const nomeVeiculo = $(this).parent().find('h3').text();
+
+        $('#veiculo').val(nomeVeiculo);
+
+        $('html').animate({
+            scrollTop: destino.offset().top
+        },1000)
+    })
+})
